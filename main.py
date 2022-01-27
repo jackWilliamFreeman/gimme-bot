57946db3afe57fad4a0355dc461ebbd1c7021d7b
William Freeman <wfreeman@fmgl.com.au>
2022-01-27 09:57:10 +0800

    added files

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if '!gimme'.encode('ascii', 'ignore') in message.content.lower().encode('ascii', 'ignore') and message.guild:
+        await message.reply('Right Away Sire!', files = [discord.File('man.jpg')])
+
+client.run(os.getenv('BOT_TOKEN'))
